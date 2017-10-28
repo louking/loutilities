5afd22734bbf4d4e8a5f753ffdc249c3aa37f8c6
Lou King <lking@pobox.com>
2017-10-28 07:04:19 -0400

    textreader.py: add TextDictReader
    * add TextDictReader class
    * allow file data to be provided through list, in addition to name of file to open

--- textreader.py
+++ textreader.py
@@ -39,9 +39,196 @@ import csv
 import version
 from loutilities import *
 
+# valid file types
 VALIDFTYPES = ['xls','xlsx','docx','txt','csv']
+# valid list types
+VALIDLTYPES = ['txt','csv']
+
 DOCXTABSIZE = 8
 TXTABSIZE = 8
+
+# exceptions for this module.  See __init__.py for package exceptions
+class headerError(Exception): pass
+
+########################################################################
+class TextDictReader():
+########################################################################
+    '''
+    get data from a file in various formats
+    
+    :param filename: name of file to open, or list-like
+    :param fieldxform: {'fieldname1':['hdr11, hdr12, ...'], 'fieldname2':[hdr21, ...], ...}, where fieldnamex will be returned in dict, hdrxx are possible headers for columns
+    :param reqdfields: list of fields required to be in the header
+    :param filetype: if filename is list, this should be filetype list should be interpreted as
+    '''
+    #----------------------------------------------------------------------
+    def __init__(self, filename, fieldxform, reqdfields, filetype='extension'):
+    #----------------------------------------------------------------------
+        # open the textreader using the file
+        self.file = TextReader(filename, filetype)
+        self.fieldxform = fieldxform
+        self.reqdfields = reqdfields
+        
+        # self.field item value will be of form {'begin':startindex,'end':startindex+length} for easy slicing
+        self.field = {}
+
+        # scan to the header line
+        self._findhdr()
+
+    #----------------------------------------------------------------------
+    def _findhdr(self):
+    #----------------------------------------------------------------------
+        '''
+        find the header in the file
+        '''
+    
+        foundhdr = False
+        delimited = self.file.getdelimited()
+        fields = self.fieldxform.keys()
+        MINMATCHES = len(self.reqdfields)
+
+        # catch StopIteration, which means header wasn't found in the file
+        try:
+            # loop for each line until header found
+            while True:
+                origline = self.file.next()
+                fieldsfound = 0
+                line = []
+                if not delimited:
+                    for word in origline.split():
+                        line.append(word.lower())
+                else:
+                    for word in origline:
+                        line.append(str(word).lower())  # str() called in case non-string returned in origline
+                    
+                # loop for each potential self.field in a header
+                for fieldndx in range(len(fields)):
+                    f = fields[fieldndx]
+                    match = self.fieldxform[f]
+                    
+                    # loop for each match possiblity, then within the fields in file header
+                    # folding loop likes this gives precedence to the list order of the match possibilities
+                    for m in match:
+                        # loop for each column in this line, trying to find match possiblities
+                        matchfound = False
+                        for linendx in range(len(line)):                        
+                            # match over the end of the line is no match
+                            # m is either a string or a list of strings
+                            if type(m) == str:
+                                m = [m]         # make single string into list
+                            if linendx+len(m)>len(line):
+                                continue
+                            # if we found the match, remember start and end of match
+                            if line[linendx:linendx+len(m)] == m:
+                                if f not in self.field: self.field[f] = {}
+                                self.field[f]['start'] = linendx
+                                self.field[f]['end'] = linendx + len(m)
+                                self.field[f]['match'] = m
+                                self.field[f]['genfield'] = f   # seems redundant, but [f] index is lost later in self.foundfields
+                                fieldsfound += 1
+                                matchfound = True
+                                break   # match possibility loop
+                        
+                        # found match for this self.field
+                        if matchfound: break
+                
+                # here we've gone through each self.field in the line
+                # need to match more than MINMATCHES to call it a header line
+                if fieldsfound >= MINMATCHES:
+                    
+                    # verify that all other required fields are present
+                    fieldsnotfound = []
+                    for f in self.reqdfields:
+                        if f not in self.field:
+                            fieldsnotfound.append(f)
+                    if len(fieldsnotfound) != 0:
+                        raise headerError, 'could not find fields {} in header {}'.format(fieldsnotfound,origline)
+                        
+                    # sort found fields by order found within the line
+                    foundfields_dec = [(self.field[f]['start'],self.field[f]) for f in self.field]
+                    foundfields_dec.sort()
+                    self.foundfields = [ff[1] for ff in foundfields_dec] # get rid of sorting decorator
+                        
+                    # here we have decided it is a header line
+                    # if the file is not delimited, we have to find where these fields start
+                    # and tell self.file where the self.field breaks are
+                    # assume multi self.field matches are separated by single space
+                    if not delimited:
+                        # sort found fields by 'start' linendx (self.field number within line)
+                        # loop through characters in original line, skipping over spaces within matched fields, to determine
+                        # where delimiters should be
+                        delimiters = []
+                        thischar = 0
+                        foundfields_iter = iter(self.foundfields)
+                        thisfield = next(foundfields_iter)
+                        while True:
+                            # scan past the white space
+                            while thischar < len(origline) and origline[thischar] == ' ': thischar += 1
+                            
+                            # we're done looking if we're at the end of the line
+                            if thischar == len(origline): break
+                            
+                            # found a word, remember where it was
+                            delimiters.append(thischar)
+                            
+                            # look for the next match of known header fields
+                            matchfound = False
+                            if thisfield is not None:
+                                # if a match, might be multiple words.  Probably ok to assume single space between them
+                                fullmatch = ' '.join(thisfield['match'])
+                                if origline[thischar:thischar+len(fullmatch)].lower() == fullmatch:
+                                    thischar += len(fullmatch)
+                                    matchfound = True
+                                    try:
+                                        thisfield = next(foundfields_iter)
+                                    except StopIteration:
+                                        thisfield = None
+                            
+                            # if found a match, thischar is already updated.  Otherwise, scan past this word
+                            if not matchfound:
+                                while thischar < len(origline) and origline[thischar] != ' ': thischar += 1
+                            
+                            # we're done looking if we're at the end of the line
+                            if thischar == len(origline): break
+                        
+                        # set up delimiters in the file reader
+                        self.file.setdelimiter(delimiters)
+                                    
+                    break
+
+            # header fields are in foundfields
+            # need to figure out the indeces for data which correspond to the foundfields
+            self.fieldhdrs = []
+            self.fieldcols = []
+            skipped = 0
+            for f in self.foundfields:
+                self.fieldhdrs.append(f['genfield'])
+                currcol = f['start'] - skipped
+                self.fieldcols.append(currcol)
+                skipped += len(f['match']) - 1  # if matched multiple columns, need to skip some
+                
+        # not good to come here
+        except StopIteration:
+            raise headerError, 'header not found'
+        
+    #----------------------------------------------------------------------
+    def next(self):
+    #----------------------------------------------------------------------
+        '''
+        return dict with generic headers and associated data from file
+        '''
+        
+        # get next raw line from the file
+        rawline = self.file.next()
+        
+        # pick columns which are associated with generic headers
+        filteredline = [rawline[i] for i in range(len(rawline)) if i in self.fieldcols]
+        
+        # create dict association, similar to csv.DictReader
+        result = dict(zip(self.fieldhdrs,filteredline))
+                   
+        # and return result
+        return result
 
 ########################################################################
 class TextReader():
@@ -50,20 +237,31 @@ class TextReader():
     abstract text reader for several different file types
     creation of object opens TextReader file
     
-    :param filename: name of file to open
+    :param filename: name of file to open, or list
     '''
 
     #----------------------------------------------------------------------
-    def __init__(self,filename):
+    def __init__(self, filename, filetype='extension'):
     #----------------------------------------------------------------------
         """
         open TextReader file
         
-        :param filename: name of file to open
+        :param filename: name of file to open, or list-like
+        :param filetype: if filename is list, this should be filetype list should be interpreted as
         """
-        self.ftype = filename.split('.')[-1] # get extension
-        if self.ftype not in VALIDFTYPES:
-            raise parameterError, 'Invalid filename {0}: must have extension in {1}'.format(filename,VALIDFTYPES)
+
+        # if true filename, type of filename is string
+        if type(filename) == str:
+            self.ftype = filename.split('.')[-1].lower() # get extension
+            self.intype = 'file'
+            if self.ftype not in VALIDFTYPES:
+                raise parameterError, 'Invalid filename {0}: must have extension in {1}'.format(filename,VALIDFTYPES)
+        # otherwise assume 'filename' is list-like
+        else:
+            self.ftype = filetype.lower()
+            self.intype = 'list'
+            if self.ftype not in VALIDLTYPES:
+                raise parameterError, 'Invalid list: must use filetype in {1}'.format(VALIDLTYPES)
         
         # handle excel files
         if self.ftype in ['xls','xlsx']:
@@ -84,12 +282,18 @@ class TextReader():
             
         # handle txt files
         elif self.ftype in ['txt']:
-            self.TXT = open(filename,'r')
+            if self.intype == 'file':
+                self.TXT = open(filename,'r')
+            else:
+                self.TXT = iter(filename)
             self.delimited = False
             
         # handle txt files
         elif self.ftype in ['csv']:
-            self._CSV = open(filename,'rb')
+            if self.intype == 'file':
+                self._CSV = open(filename,'rb')
+            else:
+                self._CSV = iter(filename)
             self.CSV = csv.reader(self._CSV)
             self.delimited = True
             
@@ -153,8 +357,12 @@ class TextReader():
         
         self.opened = False
         
+        # handle lists
+        if self.intype == 'list':
+            pass    # no resources to release
+
         # handle excel files
-        if self.ftype in ['xls','xlsx']:
+        elif self.ftype in ['xls','xlsx']:
             pass    # resources have already been released
         
         # handle word files
