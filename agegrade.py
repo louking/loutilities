louking/loutilities

commit 9be4809141f887b51f1089093279e67a17d4797c

agegrade.py: round dist to nearest meter before table lookup
* also round down returned agegrade to 2 decimal places
* fixes #7

agegrade.py
@@ -34,6 +34,7 @@ import csv
 import pickle
 import os.path
 import shutil
+from math import floor
 
 # pypi
 
@@ -90,6 +91,7 @@ def getagtable(agegradewb):
         for r in sheet:
             if r['dist(km)'] == '0.0': continue
             
+            # dist is rounded to the nearest meter so it can be used as a key
             dist = int(round(float(r['dist(km)'])*1000))
 
             # kludge to use only road events -- affects distances 5km and beyond (issue #55)
@@ -161,6 +163,12 @@ class AgeGrade():
         :rtype: (factor, openstd) - factor (age grade factor) is between 0 and 1, openstd (open standard) is in seconds
         '''
         
+        # round distmeters to the nearest meter as distlist keys are rounded to the nearest meter
+        # need to do this so exact match doesn't get interpolated
+        # alternate, possibly better, solution would be to use keys in millimeters rather than meters
+        # but that would require reloading the data -- that might be fine, but is a bit risky
+        distmeters = int(round(distmeters))
+
         # find surrounding Xi points, and corresponding Fi, OCi points
         distlist = self.agegradedata[gen].keys()
         distlist.sort()
@@ -254,7 +262,8 @@ class AgeGrade():
                 openstd = openstd1 + (1.0*(age-age1)/(age2-age1))*(openstd2-openstd1)
         
         # return age grade statistics
-        agpercentage = 100*(openstd/factor)/time
+        # use floor function to 2 decimal place percentage to emulate http://www.howardgrubb.co.uk/athletics/wmalookup15.html
+        agpercentage = floor(100*100*(openstd/factor)/time)/100
         agresult = time*factor
         if self.DEBUG:
             # order must match header written in self.__init__

commit fadc718b2fdd7c6a86b4a65ddb5a441f5ae2138a

agegrade: interpolation now uses float arithmetic (regression)
* fixes rrwebapp#334
* in 9be4809141f887b51f1089093279e67a17d4797c mistakenly made dist an integer, causing miscalculation during interpolation

agegrade.py
@@ -167,7 +167,8 @@ class AgeGrade():
         # need to do this so exact match doesn't get interpolated
         # alternate, possibly better, solution would be to use keys in millimeters rather than meters
         # but that would require reloading the data -- that might be fine, but is a bit risky
-        distmeters = int(round(distmeters))
+        # don't use int because need float arithmetic for interpolate
+        distmeters = round(distmeters)
 
         # find surrounding Xi points, and corresponding Fi, OCi points
         distlist = self.agegradedata[gen].keys()

textreader.py
@@ -251,17 +251,17 @@ class TextReader():
         """
 
         # if true filename, type of filename is string
-        if type(filename) == str:
+        if type(filename) in [str,unicode]:
             self.ftype = filename.split('.')[-1].lower() # get extension
             self.intype = 'file'
             if self.ftype not in VALIDFTYPES:
-                raise parameterError, 'Invalid filename {0}: must have extension in {1}'.format(filename,VALIDFTYPES)
+                raise parameterError, 'Invalid filename {}: must have extension in {}'.format(filename,VALIDFTYPES)
         # otherwise assume 'filename' is list-like
         else:
             self.ftype = filetype.lower()
             self.intype = 'list'
             if self.ftype not in VALIDLTYPES:
-                raise parameterError, 'Invalid list: must use filetype in {1}'.format(VALIDLTYPES)
+                raise parameterError, 'Invalid list: must use filetype in {}'.format(VALIDLTYPES)
         
         # handle excel files
         if self.ftype in ['xls','xlsx']:

version.py
@@ -1,2 +1,2 @@
 # this string is used for the version string in the documentation, as well as the egg
-__version__ = '0.14.0rc1'
+__version__ = '0.13.3rc1'