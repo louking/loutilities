c9198badfd58ca03d8593caa2541144fdb46e71d
Lou King <lking@pobox.com>
2018-10-17 06:11:06 -0400

    nesteddict: method updates / enhancements

--- nesteddict.py
+++ nesteddict.py
@@ -8,6 +8,11 @@
 #   Copyright 2018 Lou King
 ###########################################################################################
 
+# standard
+from copy import deepcopy
+
+class parameterError(Exception): pass
+
 #----------------------------------------------------------------------
 def dotted2nested_set(d, key, value):
 #----------------------------------------------------------------------
@@ -36,14 +41,48 @@ def dotted2nested_get(d, key):
     else:
         return dotted2nested_get(d[keysplit[0]], '.'.join(keysplit[1:]))
 
+#----------------------------------------------------------------------
+def nested2dotted_keys(d):
+#----------------------------------------------------------------------
+    keys = []
+
+    for key in d:
+        # need to drill down if we are at a dict-like object
+        if isinstance(d[key], dict):
+            keys += ['.'.join([key, subkey]) for subkey in nested2dotted_keys(d[key])]
+
+        else:
+            keys.append(key)
+
+    return keys
+
+#----------------------------------------------------------------------
+def obj2dict(obj):
+#----------------------------------------------------------------------
+    # adapted from https://stackoverflow.com/questions/7963762/what-is-the-most-economical-way-to-convert-nested-python-objects-to-dictionaries
+    if not  hasattr(obj,"__dict__"):
+        return obj
+    result = {}
+    for key, val in obj.__dict__.items():
+        if key.startswith("_"):
+            continue
+        element = []
+        if isinstance(val, list):
+            for item in val:
+                element.append(obj2dict(item))
+        else:
+            element = obj2dict(val)
+        result[key] = element
+    return result
+
 ###########################################################################################
 class NestedDict(dict):
 ###########################################################################################
 
     #----------------------------------------------------------------------
-    def __init__(self):
+    def __init__(self, val={}):
     #----------------------------------------------------------------------
-        self.val = {}
+        self.set(val)
 
     #----------------------------------------------------------------------
     def __setitem__(self, key, val):
@@ -56,7 +95,39 @@ class NestedDict(dict):
         return dotted2nested_get(self.val, key)
 
     #----------------------------------------------------------------------
+    def set(self, newval):
+    #----------------------------------------------------------------------
+        '''
+        set val of class to supplied parameter
+
+        if dict just copy
+        convert object to dict
+        raise error if not dict or object
+
+        parameters:
+
+        * newval - must be dict or obj, otherwise exception
+        '''
+        if isinstance(newval, dict):
+            self.val = deepcopy(newval)
+
+        elif hasattr(newval, '__dict__'):
+            self.val = obj2dict(newval)
+
+        else:
+            raise parameterError, 'invalid parameter {}: newval must be dict or object'.format(newval)
+
+    #----------------------------------------------------------------------
     def to_dict(self):
     #----------------------------------------------------------------------
         return self.val
 
+    #----------------------------------------------------------------------
+    def to_dotted(self):
+    #----------------------------------------------------------------------
+        dotted = {}
+
+        for key in nested2dotted_keys(self.val):
+            dotted[key] = dotted2nested_get(self.val, key)
+
+        return dotted
