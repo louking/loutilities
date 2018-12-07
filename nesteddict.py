4a239116e4b9de09639b44e416b8e44799ef42a0
Lou King <lking@pobox.com>
2018-12-07 12:32:57 -0500

    nesteddict.py: add Dictate class

--- nesteddict.py
+++ nesteddict.py
@@ -131,3 +131,42 @@ class NestedDict(dict):
             dotted[key] = dotted2nested_get(self.val, key)
 
         return dotted
+
+###########################################################################################
+class Dictate(object):
+###########################################################################################
+    # see https://stackoverflow.com/questions/1305532/convert-nested-python-dict-to-object
+    '''
+    Object view of a dict, updating the passed in dict when values are set
+    or deleted. "Dictate" the contents of a dict...:
+    '''
+
+    def __init__(self, d):
+        # since __setattr__ is overridden, self.__dict = d doesn't work
+        object.__setattr__(self, '_Dictate__dict', d)
+
+    # Dictionary-like access / updates
+    def __getitem__(self, name):
+        value = self.__dict[name]
+        if isinstance(value, dict):  # recursively view sub-dicts as objects
+            value = Dictate(value)
+        return value
+
+    def __setitem__(self, name, value):
+        self.__dict[name] = value
+    def __delitem__(self, name):
+        del self.__dict[name]
+
+    # Object-like access / updates
+    def __getattr__(self, name):
+        return self[name]
+
+    def __setattr__(self, name, value):
+        self[name] = value
+    def __delattr__(self, name):
+        del self[name]
+
+    def __repr__(self):
+        return "%s(%r)" % (type(self).__name__, self.__dict)
+    def __str__(self):
+        return str(self.__dict)
