0b34719b32ce2d03624c324ac2b7f1faef1c8630
Lou King <lking@pobox.com>
2020-03-17 11:01:48 -0400

    base handling of <interest> url

---
+++ loutilities/tables-assets/static/user/admin/groups.js
@@ -0,0 +1,94 @@
+
+function register_group_for_editor(groupname, groupselectselector) {
+    // requires Editor
+    // expects editor to be set up globally, as in loutilities/table-assets/static/datatables.js
+    // call after datatables is initialized
+    var staticconfig;
+
+    function translate_interest(e, data, action) {
+        // group comes from external source
+        var group = $( groupselectselector ).val();
+        // note use of lodash
+        staticconfig = _.cloneDeep(editor.ajax());
+        var newconfig = _.cloneDeep(staticconfig);
+        // substitute group into urls
+        for (const action in newconfig) {
+            newconfig[action].url = _.replace(decodeURIComponent(newconfig[action].url), _.replace('<{groupname}>', '{groupname}', groupname), group);
+        }
+        editor.ajax(newconfig);
+    }
+
+    // need on 'preSubmit' to translate interest for resubmission of form, e.g., after error occurs
+    editor.on( 'preSubmit', translate_interest);
+    // need on 'open' to translate interest for file uploads, as there's no 'preSubmit' for these
+    editor.on( 'open', translate_interest);
+
+    editor.on( 'postSubmit', function(e, data, action, xhr) {
+        editor.ajax(staticconfig);
+    })
+
+}
+
+// see https://stackoverflow.com/a/18660968/799921
+function link_is_external(link_element) {
+    return (link_element.host !== window.location.host);
+}
+
+// see https://stackoverflow.com/a/5713807/799921
+function deparam(query){
+  var setValue = function(root, path, value){
+    if(path.length > 1){
+      var dir = path.shift();
+      if( typeof root[dir] == 'undefined' ){
+        root[dir] = path[0] == '' ? [] : {};
+      }
+
+      arguments.callee(root[dir], path, value);
+    }else{
+      if( root instanceof Array ){
+        root.push(value);
+      }else{
+        root[path] = value;
+      }
+    }
+  };
+  var nvp = query.split('&');
+  var data = {};
+  for( var i = 0 ; i < nvp.length ; i++ ){
+    var pair = nvp[i].split('=');
+    var name = decodeURIComponent(pair[0]);
+    var value = decodeURIComponent(pair[1]);
+
+    var path = name.match(/(^[^\[]+)(\[.*\]$)?/);
+    var first = path[1];
+    if(path[2]){
+      //case of 'array[level1]' || 'array[level1][level2]'
+      path = path[2].match(/(?=\[(.*)\]$)/)[1].split('][')
+    }else{
+      //case of 'name'
+      path = [];
+    }
+    path.unshift(first);
+
+    setValue(data, path, value);
+  }
+  return data;
+}
+
+function register_group(groupname, groupselectselector, groupargappendselector) {
+    var thisgroupappendselector = _.replace( groupargappendselector, '{groupname}', groupname );
+    $( thisgroupappendselector ).click( function(e) {
+        // don't process external links #71
+        if (link_is_external(this)) return;
+        e.preventDefault();
+        var group = $( groupselectselector ).val();
+
+        var basequery =  $(this).attr('href').split('?');
+        var args = {}
+        if (basequery.length > 1) {
+            args = deparam(basequery[1]);
+        }
+        args[groupname] = group;
+        location.href = $(this).attr('href') + '?' + $.param(args);
+    });
+}
---
+++ loutilities/user/tables.py
@@ -0,0 +1,101 @@
+'''
+tables - support tables for user package under loutilities
+==============================================================
+'''
+# pypi
+from flask import g, current_app
+from flask_security import auth_required
+from flask_security import current_user
+
+# homegrown
+from loutilities.tables import DbCrudApiRolePermissions
+from loutilities.user.model import Interest
+
+class ParameterError(Exception): pass
+
+debug = False
+
+#####################################################
+class DbCrudApiInterestsRolePermissions(DbCrudApiRolePermissions):
+    '''
+    This class extends DbCrudApiRolePermissions. This extension uses flask_security
+    to do role checking for the current user and managing Interests usage (see
+    loutilities.user
+    '''
+
+    decorators = [auth_required()]
+
+    def __init__(self, **kwargs):
+        # ----------------------------------------------------------------------
+        if debug: current_app.logger.debug('DbCrudApiInterestsRolePermissions.__init__()')
+
+        # the args dict has default values for arguments added by this derived class
+        # caller supplied keyword args are used to update these
+        # all arguments are made into attributes for self by the inherited class
+        args = dict(
+                    local_interest_model=None,
+                    )
+        args.update(kwargs)
+
+        # this initialization needs to be done before checking any self.xxx attributes
+        super().__init__(**args)
+
+        # Caller must use local_interest_model
+        if not self.local_interest_model:
+            raise ParameterError('local_interest_model required')
+
+    def permission(self):
+        '''
+        check for permission on data
+        :rtype: boolean
+        '''
+        if debug: print('DbCrudApiInterestsRolePermissions.permission()')
+
+        # need to be logged in
+        if not current_user.is_authenticated:
+            return False
+
+        # g.interest initialized in <project>.create_app.pull_interest
+        # g.interest contains slug, pull in interest db entry. If not found, no permission granted
+        self.interest = Interest.query.filter_by(interest=g.interest).one_or_none()
+        if not self.interest:
+            return False
+
+        # check permissions allowed/permitted
+        if not super().permission():
+            return False
+
+        # current_user has permissions. Can this user access current interest?
+        if self.interest in current_user.interests:
+            return True
+        else:
+            return False
+
+    def beforequery(self):
+        '''
+        filter on current interest
+        :return:
+        '''
+        # self.interest set in self.permission()
+        # need to convert to id of local interest table for query
+        self.localinterest = self.local_interest_model.query.filter_by(interest_id=self.interest.id).one()
+        self.queryparams['interest_id'] = self.localinterest.id
+
+    # ----------------------------------------------------------------------
+    def createrow(self, formdata):
+        '''
+        creates row in database
+
+        :param formdata: data from create form
+        :rtype: returned row for rendering, e.g., from DataTablesEditor.get_response_data()
+        '''
+        if debug: print('DbCrudApiInterestsRolePermissions.createrow()')
+
+        # make sure we record the row's local interest id (self.localinterest set in self.beforequery())
+        formdata['interest_id'] = self.localinterest.id
+
+        # return the row
+        row = super().createrow(formdata)
+
+        return row
+
