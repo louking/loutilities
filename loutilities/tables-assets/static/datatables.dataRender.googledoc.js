8497b08f9eba5e5a5ddbc799b6b8f2932badd36f
Lou King <lking@pobox.com>
2021-02-16 16:32:49 -0500

    tables googledocs rendering allows custom text based on row data (https://github.com/louking/members/issues/375)

--- loutilities/tables-assets/static/datatables.dataRender.googledoc.js
+++ loutilities/tables-assets/static/datatables.dataRender.googledoc.js
@@ -5,9 +5,16 @@
  */
 jQuery.fn.dataTable.render.googledoc = function ( label ) {
     return function ( d, type, row ) {
+        var linktext;
+        // if label is a function, the text for the link may depend on row information
+        if (_.isFunction(label)) {
+            linktext = label(row)
+        } else {
+            linktext = label;
+        }
         // only add link if data is present
         if ( d != "" ) {
-            return '<a target=_blank href="https://docs.google.com/document/d/' + d + '/preview">' + label + '</a>';
+            return '<a target=_blank href="https://docs.google.com/document/d/' + d + '/preview">' + linktext + '</a>';
         } else {
             return d;
         }
