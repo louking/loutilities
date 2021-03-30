22fe0c534b874d4f2c151b6eede3dfe3561105a2
Lou King <lking@pobox.com>
2021-03-30 15:19:39 -0400

    datatables tables-assets: add ability to visually separate datatable buttons (https://github.com/louking/members/issues/431)

---
+++ loutilities/tables-assets/static/editor.buttons.separator.js
@@ -0,0 +1,6 @@
+$.fn.dataTable.ext.buttons.separator = {
+    text: '&#8203;',
+    attr: {style: 'width: 5px; padding-left: 0px; padding-right: 0px; cursor: auto;'},
+    action: function () {
+    }
+};
