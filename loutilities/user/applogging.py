1834a20770d49490d78a8ca7de6b58a013af6169
Lou King <lking@pobox.com>
2021-04-07 05:18:55 -0400

    user/applogging.py: make processing generic, include logging improvements from members (https://github.com/louking/members/issues/443)

--- loutilities/user/applogging.py
+++ loutilities/user/applogging.py
@@ -1,14 +1,3 @@
-#!/usr/bin/python
-###########################################################################################
-# applogging - define logging for the application
-#
-#   Date        Author          Reason
-#   ----        ------          ------
-#   07/06/18    Lou King        Create (from https://github.com/louking/rrwebapp/blob/master/rrwebapp/applogging.py)
-#
-#   Copyright 2018 Lou King
-#
-###########################################################################################
 '''
 applogging - define logging for the application
 ================================================
@@ -20,10 +9,19 @@ from logging import Formatter
 from logging.handlers import TimedRotatingFileHandler
 from flask import current_app
 
-
 # pypi
+from flask_security import current_user
 
 # homegrown
+
+class UserFormatter(Formatter):
+    # https://flask.palletsprojects.com/en/1.1.x/logging/#injecting-request-information
+    def format(self, record):
+        if current_user.is_authenticated:
+            record.user_email = current_user.email
+        else:
+            record.user_email = 'anonymous'
+        return super().format(record)
 
 # ----------------------------------------------------------------------
 def setlogging():
@@ -32,33 +30,46 @@ def setlogging():
     # this is needed for any INFO or DEBUG logging
     current_app.logger.setLevel(logging.DEBUG)
 
+    # uncomment next two lines to debug sqlalchemy pool usage
+    # logging.basicConfig()
+    # logging.getLogger('sqlalchemy.pool').setLevel(logging.DEBUG)
+
     # # patch werkzeug logging -- not sure why this is being bypassed in werkzeug._internal._log
-    # *** for some reason the following code caued debug pin not to be shown, see https://github.com/louking/rrwebapp/issues/300
+    # *** for some reason the following code caused debug pin not to be shown, see https://github.com/louking/rrwebapp/issues/300
     # werkzeug_logger = logging.getLogger('werkzeug')
     # werkzeug_logger.setLevel(logging.INFO)
 
-    # TODO: move this to new module logging, bring in from dispatcher
     # set up logging
-    ADMINS = ['lou.king@steeplechasers.org']
+    ADMINS = current_app.config['EXCEPTION_EMAIL']
+    application = current_app.config['APP_LOUTILITY']
+    # to test, set if to True, and change run environment to production
+    # if True:
     if not current_app.debug:
-        mail_handler = SMTPHandler('localhost',
+        if 'MAIL_SERVER' in current_app.config and 'MAIL_PORT' in current_app.config:
+            mailhost = (current_app.config['MAIL_SERVER'], current_app.config['MAIL_PORT'])
+        elif 'MAIL_SERVER' in current_app.config:
+            mailhost = current_app.config['MAIL_SERVER']
+        else:
+            mailhost = 'localhost'
+        mail_handler = SMTPHandler(mailhost,
                                    'noreply@steeplechasers.org',
-                                   ADMINS, 'members exception encountered')
+                                   ADMINS, f'{application} exception encountered')
         if 'LOGGING_LEVEL_MAIL' in current_app.config:
             mailloglevel = current_app.config['LOGGING_LEVEL_MAIL']
         else:
             mailloglevel = logging.ERROR
         mail_handler.setLevel(mailloglevel)
-        mail_handler.setFormatter(Formatter('''
-        Message type:       %(levelname)s
-        Location:           %(pathname)s:%(lineno)d
-        Module:             %(module)s
-        Function:           %(funcName)s
-        Time:               %(asctime)s
+        mail_handler.setFormatter(UserFormatter('''
+Message type:       %(levelname)s
+Location:           %(pathname)s:%(lineno)d
+Module:             %(module)s
+Function:           %(funcName)s
+Time:               %(asctime)s
+User:               %(user_email)s
 
-        Message:
+Message:
 
-        %(message)s
+%(message)s
         '''))
         current_app.logger.addHandler(mail_handler)
         current_app.config['LOGGING_MAIL_HANDLER'] = mail_handler
@@ -78,8 +89,8 @@ def setlogging():
             current_app.logger.addHandler(file_handler)
             current_app.config['LOGGING_FILE_HANDLER'] = file_handler
 
-            file_handler.setFormatter(Formatter(
-                '%(asctime)s %(levelname)s: %(message)s '
+            file_handler.setFormatter(UserFormatter(
+                '%(asctime)s %(levelname)s: %(user_email)s %(message)s '
                 '[in %(pathname)s:%(lineno)d]'
             ))
 
