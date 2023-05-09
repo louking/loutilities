1f7c5cd69badd323846724101ff75d1b11e9e700
Matthew Rydzik <matthew.rydzik@gmail.com>
2023-05-09 05:42:33 -0400

    Add attachments to mail helper
    Add parameter to include attachments with emails

--- loutilities/flask_helpers/mailer.py
+++ loutilities/flask_helpers/mailer.py
@@ -7,11 +7,12 @@ mailer - send email
 # pypi
 from flask import current_app
 from flask_mail import Message
+import mimetypes
 
 debug = False
 
 #----------------------------------------------------------------------
-def sendmail(subject, fromaddr, toaddr, html, text='', ccaddr=None, replytoaddr=None):
+def sendmail(subject, fromaddr, toaddr, html, text='', ccaddr=None, replytoaddr=None, attachments=[]):
 #----------------------------------------------------------------------
     '''
     send mail
@@ -23,6 +24,7 @@ def sendmail(subject, fromaddr, toaddr, html, text='', ccaddr=None, replytoaddr=
     :param text: optional text alternative to send
     :param ccaddr: optional cc address to use, may be list of addresses or comma separated
     :param replytoaddr: optional reply_to address to use, may be list of addresses or comma separated
+    :param attachments: optional attachments to include, list of Dict containing {'filename': 'text-name', 'data': 'file-data'}
     :returns: response from flask_mail.send
     '''
     stubbed = current_app.config.get('MAIL_SUPPRESS_SEND', current_app.config.get('TESTING', False))
@@ -42,6 +44,10 @@ def sendmail(subject, fromaddr, toaddr, html, text='', ccaddr=None, replytoaddr=
         body=text,
     )
 
+    for attachment in attachments:
+        if all(k in attachment for k in ("filename","data")):
+            message.attach(attachment['filename'], mimetypes.guess_type(attachment['filename'])[0],attachment['data']);
+ 
     mail.send(message)
 
     if debug: current_app.logger.debug('sendmail(): message.html={}'.format(message.html))
