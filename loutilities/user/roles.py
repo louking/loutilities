898913f5ee38965864f2173fc00a9b844a7b469b
Lou King <lking@pobox.com>
2025-07-02 12:07:26 -0400

    roles: add ROLE_AWARDS_ADMIN

--- loutilities/user/roles.py
+++ loutilities/user/roles.py
@@ -25,6 +25,7 @@ ROLE_MEETINGS_ADMIN = 'meetings-admin'
 ROLE_MEETINGS_MEMBER = 'meetings-member'
 ROLE_RACINGTEAM_ADMIN = 'racingteam-admin'
 ROLE_RACINGTEAM_MEMBER = 'racingteam-member'
+ROLE_AWARDS_ADMIN = 'awards-admin'
 roles_members = [
     {'name': ROLE_LEADERSHIP_ADMIN, 'description': 'access to leadership tasks for members application', 'apps':[APP_MEMBERS]},
     {'name': ROLE_LEADERSHIP_MEMBER, 'description': 'user of leadership tasks for members application', 'apps':[APP_MEMBERS]},
@@ -33,6 +34,7 @@ roles_members = [
     {'name': ROLE_MEETINGS_MEMBER, 'description': 'user of meetings for members application', 'apps':[APP_MEMBERS]},
     {'name': ROLE_RACINGTEAM_ADMIN, 'description': 'access to racingteam administration for members application', 'apps':[APP_MEMBERS]},
     {'name': ROLE_RACINGTEAM_MEMBER, 'description': 'user of racingteam module for members application', 'apps':[APP_MEMBERS]},
+    {'name': ROLE_AWARDS_ADMIN, 'description': 'access to awards administration for members application', 'apps':[APP_MEMBERS]},
 ]
 
 # routes roles
